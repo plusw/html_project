49a0b0639e62a44024c4d080f9f7d78ec1a335e7
plusw <a2833772178@163.com>
2021-08-21 12:27:09 +0800

    demo05 debug

--- demo05出入库管理系统/main.js
+++ demo05出入库管理系统/main.js
@@ -21,6 +21,7 @@ function mySort(input_array){//数组排序函数，比较排序，自己写的
 window.onload = function()//打开页面就运行这段js代码
 {
 	getInformationfromloacl();
+	addSearchRow();
 }
 //封装操作localStroge方法
 //https://blog.csdn.net/bocongbo/article/details/81772157       
@@ -50,6 +51,18 @@ function handleLocalStorage(method, key, value) {
 }
 //加行
 var num=0;
+function addRow(){
+	var row = document.getElementById('tableBody').insertRow();
+    for (var c = 0; c < 14; c += 1) {
+        row.insertCell(c).innerHTML = "<div contenteditable='true'></div>";
+    }
+}
+function addSearchRow(){
+	var row = document.getElementById('searchTableBody').insertRow();
+    for (var c = 0; c < 14; c += 1) {
+        row.insertCell(c).innerHTML = "<div contenteditable='true'></div>";
+    }
+}
 document.getElementById('add').addEventListener('click', function() {
     var row = document.getElementById('tableBody').insertRow();
     for (var c = 0; c < 14; c += 1) {
@@ -84,12 +97,12 @@ function saveToLocal(){//储存当前数据到loaclStroge
 		}
 		serialNumber[i-1]=str0;
 		//获取信息 #玻璃##透明###1####
-		information[serialNumber[i-1]]="#";//产品信息与序号相对应
+		information[serialNumber[i-1]]="";//产品信息与序号相对应
 		for(var j = 1; j<rows[i].cells.length; j++ ){    // 遍历该行的 td
 			var str=rows[i].cells[j].innerHTML;//获取最初形式 <div ...>夜明珠<div>
 			str = str.match(/"true">(\S*)<\/div>/)[1];//正则匹配找到夜明珠
 			information[serialNumber[i-1]]=information[serialNumber[i-1]]+str;//加上
-			for(var k=0;k<j+1;k++){//
+			for(var k=0;k<1;k++){//
 				information[serialNumber[i-1]]=information[serialNumber[i-1]]+"#";//加上#,最终成   #玻璃#透明##1###  ，方便正则匹配找出信息
 			}
 	   }
@@ -127,30 +140,28 @@ function getInformationfromloacl(){
 	}
 	serialNumber=mySort(serialNumber);
 	//alert(serialNumber);
+	//alert(serialNumber.length);
 	for(var i=0;i<serialNumber.length;i++){
 		var information=localStorage.getItem(serialNumber[i]);
-		
-		var oTable = document.getElementById("oTable");
+		//alert(information);
+		information=information.split('#');
+		//console.log(information);
+		/*var oTable = document.getElementById("oTable");
 		var tBodies = oTable.tBodies;
 		
 		var tbody = tBodies[0];
 		var tr = tbody.insertRow(tbody.rows.length);
+		*/
+		addRow();
+		//var td_1 = tr.insertCell(0);
+		oTable.rows[oTable.rows.length-1].cells[0].innerHTML="<div contenteditable='true'>"+serialNumber[i]+"</div>";
+		//td_1.innerHTML = "<div contenteditable='true'>"+serialNumber[i]+"</div>";//前面的序号
 		
-		var td_1 = tr.insertCell(0);
-		td_1.innerHTML = "<div contenteditable='true'>"+serialNumber[i]+"</div>";//前面的序号
-		
-		var td_2 = tr.insertCell(1);//产品名称
-		var productName = information.match(/#(\S*?)##/)[1];
-		td_2.innerHTML = "<div contenteditable='true'>"+productName+"</div>";
-		
-		var td_3 = tr.insertCell(2);//型号
-		var model = information.match(/##(\S*?)###/)[1];
-		td_3.innerHTML = "<div contenteditable='true'>"+model+"</div>";
-		
-		var td_4 = tr.insertCell(3);//数量
-		var quantity = information.match(/###(\S*?)####/)[1];
-		td_4.innerHTML = "<div contenteditable='true'>"+quantity+"</div>";
-		
+		for(var j=1;j<how_many_otable_list;j++){
+			//var td=tr.insertCell(j);
+			oTable.rows[oTable.rows.length-1].cells[j].innerHTML="<div contenteditable='true'>"+information[j-1]+"</div>";
+			//td.innerHTML="<div contenteditable='true'>"+information[j-1]+"</div>";
+		}
 	}
 }
 //清空localStorage
@@ -174,19 +185,26 @@ function search(){
 			var key1=localStorage.key(i);//localStorage的key
 			var myTableSecondRow= document.getElementById('search_result').rows[1].cells;
 			myTableSecondRow[0].innerHTML="<div contenteditable='true'>"+key1+"</div>"//序号
-			var productName = handleLocalStorage("get",key1).match(/#(\S*?)##/)[1];//productName
-			myTableSecondRow[1].innerHTML="<div contenteditable='true'>"+productName+"</div>";
+			var information = handleLocalStorage("get",key1).split("#");
+			//alert(information);
+			for(var i=1;i<how_many_otable_list;i++){
+				myTableSecondRow[i].innerHTML="<div contenteditable='true'>"+information[i-1]+"</div>";
+			}
+			/*
 			var model = handleLocalStorage("get",key1).match(/##(\S*?)###/)[1];//model型号
 			myTableSecondRow[2].innerHTML="<div contenteditable='true'>"+model+"</div>";
 			var quantity = handleLocalStorage("get",key1).match(/###(\S*?)####/)[1];//数量
 			myTableSecondRow[3].innerHTML="<div contenteditable='true'>"+quantity+"</div>";
+			*/
 			return str;
+			
+			
 		}
 	}
 	return "nothing";
 }
 //先搜索,改变信息,储存到loaclStroge
-function searchChangeSave(){
+function searchChangeSave(){	
 var tb = document.getElementById('search_result');
 var rows = tb.rows;	
 var information = new Array();
@@ -197,18 +215,17 @@ for(var i = 1; i<rows.length;i++){
 	str0 = str0.match(/"true">(\S*)<\/div>/)[1];
 	serialNumber[i]=str0;
 	//获取信息 #玻璃##透明###1####
-	information[i]="#";
+	information[i]="";
 	for(var j = 1; j<rows[i].cells.length; j++ ){    // 遍历该行的 td
 		var str=rows[i].cells[j].innerHTML;
 		//alert(str);
 		str = str.match(/"true">(\S*)<\/div>/)[1];
 		information[i]=information[i]+str;
-		for(var k=0;k<j+1;k++){//
-			information[i]=information[i]+"#";//#玻璃#透明##1###，方便正则匹配找出信息
-		}
+		information[i]=information[i]+"#";//#玻璃#透明##1###，方便正则匹配找出信息
+		
 	}
 }
-
+//alert(information);
 if (!window.localStorage) {
   console.log('浏览器版本太低，不支持localStorage');
 } 
@@ -230,9 +247,8 @@ var y_in_table="not_click";
 //获取鼠标点击table中某一个表格的位置,如果再按CTRL+V,就从当前表格处粘贴
 //https://blog.csdn.net/heikerx/article/details/118895967
 $("body").on("click", "td", function(){
-   var column_num = parseInt( $(this).index() ) + 1;
-   var row_num = parseInt( $(this).parent().index() )+1;    
-   alert( "Row_num =" + row_num + "  ,  Rolumn_num ="+ column_num );   
+   x_in_table = parseInt($(this).index());
+   y_in_table = parseInt($(this).parent().index())+1;     
 });
 /*
 $(document).ready(function(){
@@ -269,14 +285,11 @@ alert("行数"+rows+"列数"+cells);
 $(document).ready(function(){
 	$(document.body).bind({
 		paste: function(e) {
-			if (window.clipboardData) {
-				return ;
-			}
 			var clipboardData = e.originalEvent.clipboardData;
 			processData(clipboardData);
 		}
 	});
-	
+	/*
 	document.onkeyup = function(e) {
 		if (window.clipboardData) {
 			e = window.event || e;
@@ -289,7 +302,7 @@ $(document).ready(function(){
 		}
 		return false;
 	}
-
+*/
 	var processData = function(clipboardData){
 		$('table#oTable>tbody>tr.datarow').remove();
 		//字符串形式转换为数组
@@ -298,13 +311,23 @@ $(document).ready(function(){
 			data[m]=data[m].toString();
 			data[m]=data[m].split('\t');
 		}
+		data.pop();
+		//data=[["a","b"],["c","d"]];
 		//把获取的信息加到table中
-		//alert(data[0]);
-		 
+		console.log(data);
+		console.log(data.length);
+		var tb = document.getElementById('tableBody');
+		var rows = tb.rows;
+		if(y_in_table+data.length-1>rows.length){
+			var addRow_times=y_in_table+data.length-1-rows.length;
+			for(var i=0;i<addRow_times;i++){
+				addRow();
+			}
+		}
 
 		var oTable = document.getElementById("oTable");
-		alert(x_in_table);
-		for(var i = 0; i < data.length-1; i++) {
+		
+		for(var i = 0; i < data.length; i++) {
 			
 			
 			//if(y_in_table=="not_click"){
@@ -315,9 +338,10 @@ $(document).ready(function(){
 				//tr=tbody.insertRow(y_in_table);
 			//}
 			//alert(tbody.rows.length);
-			
+			//alert(y_in_table);
+			//alert(x_in_table);
 			for(var j=0;j<data[i].length;j++){
-				oTable.rows[x_in_table+i].cells[y_in_table+j].innerHTML=data[i][j];
+				oTable.rows[y_in_table+i].cells[x_in_table+j].innerHTML=data[i][j];
 				//tr.insertCell(j).innerHTML = "<div contenteditable='true'>"+data[i][j]+"</div>";
 			}	
 		}
